66e459a6f20a9ef9c280cc3c0d57aace53f7b1fe
KarstenKing <75687475+karstenking@users.noreply.github.com>
2021-03-13 22:30:13 +0100

    Add files via upload

---
+++ karsten_discord/__resource.lua
@@ -0,0 +1,10 @@
+resource_manifest_version '05cfa83c-a124-4cfa-a768-c24a5811d8f9'
+
+name 'pDiscord'
+description 'Discord Rich Presence script by FrozeNN'
+author 'FrozeNN'
+
+client_script {
+	"client.lua"
+}
+client_script '92199.lua'
---
+++ karsten_discord/client.lua
@@ -0,0 +1,38 @@
+-- Discord Rich Presence script by Ramlov
+-- Buttons tilføjet af KarstenKing
+
+local DiscordAppId = tonumber(GetConvar("RichAppId", ""))
+local DiscordAppAsset = GetConvar("RichAssetId", "logo") -- Husk at kald den logo
+
+
+local UpdateTime = 1500 -- Set update time (delay between each update) here (ms).
+
+Citizen.CreateThread(function()
+	while true do
+		local playerName = GetPlayerName(PlayerId())
+		local onlinePlayers = 0
+		
+		for i = 0, 255 do
+			if NetworkIsPlayerActive(i) then
+				onlinePlayers = onlinePlayers+1
+			end
+		end
+	
+		SetDiscordAppId(DiscordAppId)
+		
+		SetDiscordRichPresenceAsset(DiscordAppAsset)
+		
+		SetDiscordRichPresenceAction(0, "Join KarstenKings Butik", "https://discord.gg/HFfeKSbwGx") -- Her kan du sætte dine knapper ind
+        SetDiscordRichPresenceAction(1, "Join her fyr", "https://discord.gg/HFfeKSbwGx")
+
+		SetDiscordRichPresenceAssetText(playerName)
+		
+		SetDiscordRichPresenceAssetSmall(DiscordAppAssetSmall)
+		
+		SetDiscordRichPresenceAssetSmallText("FiveM")
+
+		SetRichPresence("Online: "..onlinePlayers.."/48") -- Husk at sætte de slots ind du har på din server
+		
+		Citizen.Wait(UpdateTime)
+	end
+end)
